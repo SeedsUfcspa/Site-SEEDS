41223af49eaa47a541088bc2a169e47dd52d1694
Handriel Scheffer <handriel.scheffer@mca.ufcspa.edu.br>
2025-09-11 16:10:53 -0300

    feat: Adiciona componente Extensão

---
+++ frontend/src/app/components/extensao/extensao.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { TuiAvatar } from '@taiga-ui/kit';
+
+
+@Component({
+  selector: 'app-extensao',
+  standalone: true,
+  imports: [TuiAvatar],
+  templateUrl: './extensao.html',
+  styleUrl: './extensao.scss'
+})
+export class ExtensaoComponent {
+
+}
